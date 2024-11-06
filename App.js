25e799f65503a92abc25c31c8f3873a30fde47bd
smiles_777 <p.smiles777@gmail.com>
2024-11-06 11:31:32 +0900

    add iOS build

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Prevent the splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  useEffect(() => {
+    // Simulate a loading process or async task before hiding the splash screen
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 2000); // Adjust the delay as needed
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/image.png')} // Ensure the image path matches your project structure
+        style={styles.logo}
+      />
     </View>
   );
 }
@@ -13,8 +27,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF3B00', // Red background
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 430, // Adjust dimensions as needed
+    height: 932,
+    resizeMode: 'contain',
+  },
 });
+
